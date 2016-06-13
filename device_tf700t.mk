aacc7398938d89ed46f8b0d92c067ff71a69f6f4
Rick Ucker <rick.ucker@gmail.com>
2016-06-12 22:10:02 -0400

    Remove dep on media_codecs_ffmpeg.xml

--- device_tf700t.mk
+++ device_tf700t.mk
@@ -118,7 +118,6 @@ PRODUCT_PROPERTY_OVERRIDES := \
 
 # media files
 PRODUCT_COPY_FILES += \
-    frameworks/av/media/libstagefright/data/media_codecs_ffmpeg.xml:system/etc/media_codecs_ffmpeg.xml \
     frameworks/av/media/libstagefright/data/media_codecs_google_audio.xml:system/etc/media_codecs_google_audio.xml \
     frameworks/av/media/libstagefright/data/media_codecs_google_video.xml:system/etc/media_codecs_google_video.xml \
     device/asus/tf700t/configs/media_codecs.xml:system/etc/media_codecs.xml \
